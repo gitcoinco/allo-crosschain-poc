065e2c93dbde5071c4d8099d49e87fa79128182b
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-03-06 16:43:48 -0600

    chore: update script

--- scripts/initAxelar.ts
+++ scripts/initAxelar.ts
@@ -1,7 +1,21 @@
-// TODO:
+import { ethers } from "hardhat";
 
-// const AxelarTrustedReceiver = await hre.ethers.getContract("AxelarTrustedReceiver", deployer);
-// const Round = await hre.ethers.getContract("Round", deployer);
+async function main() {
 
-// await AxelarTrustedReceiver.init(Round.address, ethers.constants.AddressZero);
-// await Round.setTrustedReceiver(AxelarTrustedReceiver.address);
+    const roundContract = "";
+    const round= await ethers.getContractAt("Round", roundContract);
+    
+    const axelarTrustedReceiverContract = "";
+    const axelarTrustedReceiver = await ethers.getContract("AxelarTrustedReceiver", axelarTrustedReceiverContract);
+    
+    await round.setTrustedReceiver(axelarTrustedReceiver.address);
+
+    await axelarTrustedReceiver.init(round.address, ethers.constants.AddressZero);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
