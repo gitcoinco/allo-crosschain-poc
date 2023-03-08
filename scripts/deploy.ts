b83d627e890369d8f32ff1725dea8ef33fcb1105
Andrea Franz <andrea@gravityblast.com>
2023-03-08 17:49:02 +0100

    add test deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,61 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const network = await ethers.provider.getNetwork();
+  const networkName = await hre.network.name;
+  const account = (await ethers.getSigners())[0];
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  console.log(`chainId: ${network.chainId}`);
+  console.log(`network: ${networkName} (from ethers: ${network.name})`);
+  console.log(`account: ${account.address}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Registry = await ethers.getContractFactory("Registry", account);
+  const registry = await Registry.deploy();
+  console.log("registry deployed at", registry.address);
 
-  await lock.deployed();
+  const AxelarApplicationForwarder = await ethers.getContractFactory(
+    "AxelarApplicationForwarder",
+    account
+  );
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const gatewayAddress = "0xe432150cce91c13a887f7D836923d5597adD8E31";
+  const gasReceiverAddress = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6";
+  const destinationAddress = "0xCA3597cDe46f9e932D187ec66A5f12B5CE235a81";
+
+  const forwarder = await AxelarApplicationForwarder.deploy(
+    registry.address,
+    gatewayAddress,
+    gasReceiverAddress
+  );
+  console.log("forwarder deployed at", forwarder.address);
+
+  console.log("updating forwarders...");
+  await registry.updateForwarder(forwarder.address, true);
+  console.log("done.");
+
+  console.log("creating first project...");
+  await registry.createProject();
+  console.log("done.");
+
+  console.log("creating second project...");
+  const tx = await registry.createProject();
+  console.log("done.");
+  const rec = await tx.wait();
+  const projectID = rec.logs[0].data;
+  console.log("projectID", projectID);
+
+  console.log("applyToRound...");
+  await registry.applyToRound(
+    forwarder.address,
+    {
+      projectID: projectID,
+      destinationChain: "Fantom",
+      destinationAddress,
+      value: projectID,
+    },
+    { value: ethers.utils.parseEther("0.1") }
+  );
+  console.log("done.");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
