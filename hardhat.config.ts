4a70cf047712392650cc8077ba5da45d8f04c4b7
Andrea Franz <andrea@gravityblast.com>
2023-03-08 17:47:39 +0100

    add goerli config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 // import "@nomicfoundation/hardhat-toolbox";
-// import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env["ALCHEMY_KEY_GOERLI"]}`,
+      accounts: {
+        mnemonic: process.env.TEST_MNEMONIC,
+      },
+    },
+  },
 };
 
 export default config;
